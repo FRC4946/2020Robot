9a1e27600fc6ea49ac92d8519dc42d97d0dc318b
Matthew Z <emzeegotanemail2@gmail.com>
2020-02-07 13:17:46 -0500

    Add stuff to shooter hood angle command

--- src/main/java/frc/robot/commands/ShooterHoodAngleCommand.java
+++ src/main/java/frc/robot/commands/ShooterHoodAngleCommand.java
@@ -39,6 +39,7 @@ public class ShooterHoodAngleCommand extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    m_shooter.stopBothHoodMotors();
   }
 
   // Returns true when the command should end.
