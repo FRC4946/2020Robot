08661d71813961ade3e27348a32ec7c3e3740cb8
Matthew Z <emzeegotanemail2@gmail.com>
2020-02-11 17:14:00 -0500

    Add timed climber command

--- src/main/java/frc/robot/commands/TimedClimber.java
+++ src/main/java/frc/robot/commands/TimedClimber.java
@@ -7,23 +7,25 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Climber;
 
-public class ClimberCommand extends CommandBase {
+public class TimedClimber extends CommandBase {
   /**
-   * Creates a new ClimberCommand.
+   * Creates a new TimedClimber.
    */
 
   private Climber m_climber;
+  private Timer m_climberTimer;
+  private double m_time;
   private double m_speed;
-  private double m_height;
-  private double m_distanceTravelled = 0.0;
+  private boolean m_hasTimePassed;
 
-  public ClimberCommand(Climber climber, double speed, double height) {
+  public TimedClimber(Climber climber, double speed, double time) {
     m_climber = climber;
+    m_time = time;
     m_speed = speed;
-    m_height = height;
 
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(m_climber);
@@ -32,29 +34,31 @@ public class ClimberCommand extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    m_climberTimer = new Timer();
+    m_climberTimer.start();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_distanceTravelled = m_climber.getDistance();
+    m_hasTimePassed = m_climberTimer.hasPeriodPassed(m_time);
 
-    if (m_distanceTravelled < m_height) {
+    if (!m_hasTimePassed) {
       m_climber.set(m_speed);
     } else {
       m_climber.stop();
-    } 
-  }
+      m_climberTimer.stop();
+    }
+  } 
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_climber.stop();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return (m_climber.getDistance() == m_height);
+    return false;
   }
 }
