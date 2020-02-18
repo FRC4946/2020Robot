c9246e21e307fadc87cf7dc095b82084c11cc261
Matthew Z <emzeegotanemail2@gmail.com>
2020-02-18 18:12:22 -0500

    Make the Turret subsystem a PID subsystem

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -11,22 +11,29 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 import frc.robot.Constants;
 import frc.robot.RobotMap;
 
-public class Turret extends SubsystemBase {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
+public class Turret extends PIDSubsystem {
+  /**
+   * Creates a new Turret.
+   */
 
-  private TalonSRX m_turretMotor;
-  private AnalogInput m_pot;
+  private final TalonSRX m_turretMotor;
+  private final AnalogInput m_pot;
 
-  public Turret(){
+  public Turret() {  
+    super(
+      // The PIDController used by the subsystem
+      new PIDController(Constants.PID_TURRET_P, Constants.PID_TURRET_I, Constants.PID_TURRET_D)
+    );
+
     m_turretMotor = new TalonSRX(RobotMap.CAN.TURRET_TALONSRX);
     m_pot = new AnalogInput(RobotMap.AIO.TURRET_POT);
   }
-
+  
   /**
    * Moves the Turret
    * @param speed the speed at which to set the turret motor to
@@ -45,5 +52,16 @@ public class Turret extends SubsystemBase {
   
   public double getAngle() {
     return (m_pot.getAverageVoltage() / Constants.AIO_MAX_VOLTAGE) * Constants.TURRET_POT_SCALE_VALUE * Constants.TURRET_RATIO;
+  }
+  
+  @Override
+  public void useOutput(double output, double setpoint) {
+    // Use the output here
+  }
+
+  @Override
+  public double getMeasurement() {
+    // Return the process variable measurement here
+    return 0;
   }
 }
