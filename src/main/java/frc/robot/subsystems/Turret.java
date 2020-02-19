f018a89ed4d2e1d6e7a31cd592fcaf201c29c607
Matthew Z <emzeegotanemail2@gmail.com>
2020-02-19 17:37:32 -0500

    Correct mistakes in previous commit

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -24,16 +24,16 @@ public class Turret extends PIDSubsystem {
   private final TalonSRX m_turretMotor;
   private final AnalogInput m_pot;
 
-  public Turret() {  
+  public Turret() {
     super(
-      // The PIDController used by the subsystem
-      new PIDController(Constants.PID_TURRET_P, Constants.PID_TURRET_I, Constants.PID_TURRET_D)
+        // The PIDController used by the subsystem
+        new PIDController(Constants.PID_TURRET_P, Constants.PID_TURRET_I, Constants.PID_TURRET_D)
     );
 
     m_turretMotor = new TalonSRX(RobotMap.CAN.TURRET_TALONSRX);
     m_pot = new AnalogInput(RobotMap.AIO.TURRET_POT);
   }
-  
+
   /**
    * Moves the Turret
    * @param speed the speed at which to set the turret motor to
@@ -49,19 +49,24 @@ public class Turret extends PIDSubsystem {
   public void stop(){
     m_turretMotor.set(ControlMode.PercentOutput, 0.0);
   }
-  
+
   public double getAngle() {
     return (m_pot.getAverageVoltage() / Constants.AIO_MAX_VOLTAGE) * Constants.TURRET_POT_SCALE_VALUE * Constants.TURRET_RATIO;
-  }
-  
-  @Override
-  public void useOutput(double output, double setpoint) {
-    // Use the output here
   }
 
   @Override
   public double getMeasurement() {
     // Return the process variable measurement here
     return 0;
+  }
+
+  @Override
+  protected void useOutput(double output, double setpoint) {
+    setSetpoint(setpoint);
+    set(output);
+  }
+
+  public double getSetpoint() {
+    return getSetpoint();
   }
 }
