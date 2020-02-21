c194ea92fd977f6dc3eee09022448588b4d869fa
Matthew Reynolds <rey.mtr@gmail.com>
2020-02-21 12:17:48 -0500

    Cleanup control panel commands

--- src/main/java/frc/robot/commands/colorwheel/TurnForDistance.java
+++ src/main/java/frc/robot/commands/colorwheel/TurnForDistance.java
@@ -11,55 +11,39 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ControlPanel;
 
 public class TurnForDistance extends CommandBase {
-  double m_distance;
-  double m_speed;
-  boolean m_clockwise;
-  ControlPanel m_controlPanel;
+
+  private final double m_distance;
+  private final double m_speed;
+  private final ControlPanel m_controlPanel;
 
   /**
-   * Creates a new TurnForDistance.
+   * Creates a new TurnForDistance command.
    */
-  public TurnForDistance(double distance, double speed, ControlPanel controlPanel, boolean clockwise) {
+  public TurnForDistance(double distance, double speed, ControlPanel controlPanel) {
     m_distance = distance;
     m_speed = speed;
-    m_clockwise = clockwise;
-    addRequirements(m_controlPanel);
+    m_controlPanel = controlPanel;
 
-    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_controlPanel);
   }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {
     m_controlPanel.resetEncoder();
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if(m_clockwise){
-      if(m_controlPanel.getDistance()<m_distance){
-        m_controlPanel.set(m_speed);
-      }
-    }
-    else if(!m_clockwise){
-      if(m_controlPanel.getDistance()>m_distance){
-        m_controlPanel.set(-m_speed);
-      }
-    }
-
-
+    m_controlPanel.set(m_speed);
   }
 
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     m_controlPanel.stop();
   }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return (m_clockwise) ? m_controlPanel.getDistance()>m_distance : m_controlPanel.getDistance()<m_distance;
+    return Math.abs(m_controlPanel.getDistance()) > Math.abs(m_distance);
   }
 }
--- src/main/java/frc/robot/commands/colorwheel/TurnToColor.java
+++ src/main/java/frc/robot/commands/colorwheel/TurnToColor.java
@@ -10,44 +10,34 @@ package frc.robot.commands.colorwheel;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ControlPanel;
-/**
- * Turns the control panel until the color sensor is over a specified color
- * @author jacob
- */
+
 public class TurnToColor extends CommandBase {
 
-  double m_speed;
-  Color m_color;
-  ControlPanel m_controlPanel;
+  private final double m_speed;
+  private final Color m_color;
+  private final ControlPanel m_controlPanel;
 
   /**
-   * Turns the control panel wheel, until the color sensor is over a specific color.
+   * Turns the control panel until the color sensor detects the specified color.
    */
   public TurnToColor(double speed, Color color, ControlPanel controlPanelWheel) {
     m_speed = speed;
     m_color = color;
     m_controlPanel = controlPanelWheel;
+
     addRequirements(m_controlPanel);
   }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     m_controlPanel.set(m_speed);
   }
 
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     m_controlPanel.stop();
   }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return m_controlPanel.getCurrentColor().color.equals(m_color);
