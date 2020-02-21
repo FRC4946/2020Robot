3a5a41bc81e9b7526ca14e3423c142e2137d3846
jacob4649 <jacob.klimczak@gmail.com>
2020-02-21 13:01:21 -0500

    Fix Shoot

--- src/main/java/frc/robot/commands/revolver/Shoot.java
+++ src/main/java/frc/robot/commands/revolver/Shoot.java
@@ -16,10 +16,10 @@ import frc.robot.subsystems.Turret;
 
 public class Shoot extends CommandBase {
 
-  Revolver m_revolver;
-  Shooter m_shooter;
-  Hood m_hood;
-  Turret m_turret;
+  private final Revolver m_revolver;
+  private final Shooter m_shooter;
+  private final Hood m_hood;
+  private final Turret m_turret;
 
   /**
    * Starts the revolver and feedwheel if the shooter speed, turret angle, and
@@ -27,22 +27,21 @@ public class Shoot extends CommandBase {
    * 
    * 
    * @param revolver the revolver to use for this command
-   * @param shooter  the revolver to use for this subsystem
-   * @param turret   the revolver to use for this subsystem
-   * @param hood     the revolver to use for this subsystem
+   * @param shooter  the shooter to use for this subsystem
+   * @param turret   the turret to use for this subsystem
+   * @param hood     the hood to use for this subsystem
    */
   public Shoot(Revolver revolver, Shooter shooter, Turret turret, Hood hood) {
     m_revolver = revolver;
+    m_shooter = shooter;
+    m_turret = turret;
+    m_hood = hood;
     addRequirements(m_revolver);
   }
 
   @Override
-  public void initialize() {
-  }
-
-  @Override
   public void execute() {
-    if (m_shooter.atSetpoint() && m_hood.atSetpoint() && m_shooter.atSetpoint()) {
+    if (m_shooter.atSetpoint() && m_hood.atSetpoint() && m_shooter.atSetpoint() && m_turret.atSetpoint()) {
       m_revolver.setAll(Constants.REVOLVER_DRUM_FORWARDS_SPEED, 0.3);
     } else {
       m_revolver.setAll(0.0, 0.0);
