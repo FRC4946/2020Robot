5071bb31920c4a5f67c10caaf3af1c5c361d13b3
Matthew Z <emzeegotanemail2@gmail.com>
2020-02-24 17:43:59 -0500

    Add SetHoodSpeed and SetShooterSpeed commands

---
+++ src/main/java/frc/robot/commands/hood/SetHoodSpeed.java
@@ -0,0 +1,47 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.hood;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Hood;
+
+public class SetHoodSpeed extends CommandBase {
+  private final Hood m_hood;
+  private final double m_speed;
+
+  /**
+   * Creates a new SetHoodSpeed.
+   */
+
+  public SetHoodSpeed(double speed, Hood hood) {
+    m_speed = speed;
+    m_hood = hood;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_hood.setSetpoint(m_speed);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
